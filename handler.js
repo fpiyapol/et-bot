59a08698742d8a9be920942d3523dfdf2362b0db
Piyapol <60070052@kmitl.ac.th>
2020-05-23 14:03:08 +0700

    remove sth

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ const commands = { ls: listInstances, stop: stopInstances }
 
 module.exports.handler = async (event) => {
   try {
-    const text = 'stop i-0e5833cd94bc551f1 i-0a502cd12ad9c4715'
+    const text = ''
     const [command, ...args] = text.split(' ')
     await commands[command](args)
   } catch (error) {
